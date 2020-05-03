b68240de2426b73ff204ad5c10232d0f745bcb12
Lucas Dembart <ldembart@gmail.com>
2020-05-03 13:14:54 -1000

    Commit Iteration 0

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Lucas  Dembart on 5/3/20.
+//  Copyright © 2020 Lucas  Dembart. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Lucas  Dembart on 5/3/20.
+//  Copyright © 2020 Lucas  Dembart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
