a0bb16c61b6c35ab5ffce8593befa3af32114e35
Lucas Dembart <ldembart@gmail.com>
2020-05-03 14:19:28 -1000

    Complete Iteration 1

---
+++ ToDoListApp/CompleteToDoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CompleteToDoViewController.swift
+//  ToDoListApp
+//
+//  Created by Lucas  Dembart on 5/3/20.
+//  Copyright © 2020 Lucas  Dembart. All rights reserved.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+
+    var previousVC = ToDoTableViewController()
+    var selectedToDo = ToDo()
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = selectedToDo.name
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
